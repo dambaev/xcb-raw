bc48c7f23c6db6862787aee304fbf6803f7f814c
Dambaev Alexander <dambaev.dev@gmail.com>
2017-07-15 17:36:37 +0800

    add createWindowIO function

--- src/Graphics/XCB/Raw/XCB.hs
+++ src/Graphics/XCB/Raw/XCB.hs
@@ -51,7 +51,7 @@ xcb_connectIO = do
                 return . Just . XT.Connection
 
 {-|
-    this function generates new xcb_window_t value be requesting it from
+    this function generates new xcb_window_t value by requesting it from
     server
 -}
 xcb_generate_idIO
@@ -73,4 +73,73 @@ xcb_generate_idIO (XT.Connection conn ) = do
         return $! if idOrZero == 0
             then Nothing
             else Just $ XT.XID idOrZero
+
+createWindowIO
+    :: XT.Connection
+    -> XT.CreateWindow
+    -> IO( Maybe XT.XID)
+createWindowIO xconn@(XT.Connection conn) cw = do
+    mxid <- xcb_generate_idIO xconn
+    case mxid of
+        Nothing-> return Nothing
+        Just xid@(XT.XID rawXID)-> do
+            let
+                XT.Position x' y' = XT.cwPosition cw
+                XT.Size w' h' = XT.cwSize cw
+                x = fromIntegral x'
+                y = fromIntegral y'
+                w = fromIntegral w'
+                h = fromIntegral h'
+            okOrZero <- FFP.withForeignPtr conn $! \pconn-> do [LCI.block| uint32_t
+                {
+                xcb_screen_t * p_screen = NULL;
+                xcb_connection_t * p_conn = $(void* pconn);
+                const struct xcb_setup_t * setup = xcb_get_setup(p_conn);
+                if( !setup || xcb_connection_has_error( p_conn))
+                {
+                    return 0;
+                }
+                xcb_screen_iterator_t   iter = xcb_setup_roots_iterator(setup);
+                p_screen = iter.data;
+
+                uint32_t u_values[] = { XCB_EVENT_MASK_EXPOSURE
+                                      | XCB_EVENT_MASK_STRUCTURE_NOTIFY
+                                      };
+
+                xcb_void_cookie_t cookie = xcb_create_window_checked
+                  ( p_conn
+                  , XCB_COPY_FROM_PARENT
+                  , $(uint32_t rawXID)
+                  , p_screen-> root
+                  , $(uint32_t x)
+                  , $(uint32_t y)
+                  , $(uint32_t w)
+                  , $(uint32_t h)
+                  , 0
+                  , XCB_WINDOW_CLASS_COPY_FROM_PARENT
+                  , XCB_COPY_FROM_PARENT
+                  , XCB_CW_EVENT_MASK
+                  , u_values
+                  );
+                if( xcb_request_check( p_conn, cookie)
+                 || xcb_connection_has_error( p_conn))
+                {
+                    return 0;
+                }
+                cookie = xcb_map_window( p_conn, $(uint32_t rawXID));
+                if( xcb_request_check( p_conn, cookie)
+                 || xcb_connection_has_error( p_conn))
+                {
+                    return 0;
+                }
+                if( xcb_flush( p_conn) <= 0)
+                {
+                    return 0;
+                }
+                }|]
+            return $! if okOrZero == 0
+                then Nothing
+                else Just xid
+
+
 
