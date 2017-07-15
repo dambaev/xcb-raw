ea1de986390b25c6e3d019b9be705c7b8cadd7f4
Dambaev Alexander <dambaev.dev@gmail.com>
2017-07-15 15:55:31 +0800

    1. delete unused default Lib.hs; 2. add basic data types

---
+++ src/Graphics/XCB/Raw.hs
@@ -0,0 +1,3 @@
+
+module Graphics.XCB.Raw
+  where
---
+++ src/Graphics/XCB/Raw/Types.hs
@@ -0,0 +1,70 @@
+
+module Graphics.XCB.Raw.Types
+  where
+
+import qualified Foreign.C.Types as FCT 
+import qualified Data.Word as DW
+import qualified Foreign.ForeignPtr as FFP(ForeignPtr)
+import qualified Foreign.Ptr as FP(Ptr)
+
+
+data Connection = Connection (FFP.ForeignPtr ())
+data RawEvent = RawEvent FCT.CInt (FP.Ptr ())
+
+data XID = XID DW.Word32
+  deriving ( Show
+           , Eq
+           , Ord
+           )
+
+data Event
+  = XCB_KEY_PRESS
+    { xeWindowId:: XID
+    }
+  | XCB_KEY_RELEASE
+    { xeWindowId:: XID
+    }
+  | XCB_BUTTON_PRESS
+    { xeWindowId:: XID
+    }
+  | XCB_BUTTON_RELEASE
+    { xeWindowId:: XID
+    }
+  | XCB_ENTER_NOTIFY
+    { xeWindowId:: XID
+    }
+  | XCB_LEAVE_NOTIFY
+    { xeWindowId:: XID
+    }
+  | XCB_MAP_NOTIFY
+    { xeWindowId:: XID
+    }
+  | XCB_CONFIGURE_NOTIFY
+    { xeWindowId:: XID
+    }
+  | XCB_EXPOSE
+    { xeWindowId:: XID
+    }
+  | XCB_DESTROY_NOTIFY
+    { xeWindowId:: XID
+    }
+  deriving Show
+
+data Size = Size
+  { szWidth:: Int
+  , szHeight:: Int
+  }
+
+data Position = Position
+  { posX:: Int
+  , posY:: Int
+  }
+
+{--
+ - This data structure is used in createWindow action
+-}
+data CreateWindow = CreateWindow
+  { cwParent:: Maybe XID
+  , cwSize:: Size
+  , cwPosition:: Position
+  }
